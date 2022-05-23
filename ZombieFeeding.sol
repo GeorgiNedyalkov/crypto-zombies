a82c60bfae31cf7450952830c3c44d1e0f1640db
Flu3nt <gnedyalkov94@gmail.com>
2022-05-23 15:01:16 +0300

    Func: Modified feedAndMultiply function so that it makes the new zombie have a kitty dna

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -22,18 +22,26 @@ contract ZombieFeeding is ZombieFactory {
 
     KittyInterface kittyContract = KittyInterface(ckAddress);
 
-    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+    function feedAndMultiply(
+        uint256 _zombieId, 
+        uint256 _targetDna, 
+        string memory _species) 
+        public 
+        {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         // Formula for a new zombie dna is the average between both target and zombie dnas
         _targetDna = _targetDna % dnaModulus;
         uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        if(keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty")) {
+            newDna = newDna - newDna % 100 + 99;
+        }
         _createZombie("NoName", newDna);
     }
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
         uint kittyDna;
         (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-        feedAndMultiply(_zombieId, kittyDna);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
