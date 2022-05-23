3b328a4c6e112243803054b5caf57cdcf2cee9d5
Flu3nt <gnedyalkov94@gmail.com>
2022-05-23 14:25:52 +0300

    Func: completed the first stage of ZombieFeeding contract

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -30,5 +30,10 @@ contract ZombieFeeding is ZombieFactory {
         uint256 newDna = (myZombie.dna + _targetDna) / 2;
         _createZombie("NoName", newDna);
     }
-}
 
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
+    }
+}
